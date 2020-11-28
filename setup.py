e4298d60cbd3d30d7885f87bee411be45647026a
Justin Joyce <justin.joyce@joocer.com>
2020-11-28 20:17:46 +0000

    dummy test

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+   name='orwell',
+   version='0.1.0',
+   description='Orwell',
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author='joocer',
+   author_email='justin.joyce@joocer.com',
+   packages=find_packages(),
+   url="https://github.com/joocer/orwell",
+   install_requires=[]
+)
